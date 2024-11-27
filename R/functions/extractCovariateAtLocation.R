8e066a2acfe334d7f2e4185836518bfe1b5e1d3e
Max Lindmark <max.lindmark@slu.se>
2024-11-27 21:12:29 +0100

    major revision with spatial index approach and new corrected data!

--- R/functions/extractCovariateAtLocation.R
+++ R/functions/extractCovariateAtLocation.R
@@ -4,129 +4,114 @@
 #+
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
 #+
-#+ A function to extract the mean annual value registered for 
-#+ a given covariate at set of location provided. 
+#+ A function to extract the mean annual value registered for
+#+ a given covariate at set of location provided.
 #+ It was designed to work with copernicus data obtained from
-#+ 
+#+
 #+ https://data.marine.copernicus.eu/
-#+ 
-#+ and compiled with the function 
-#+ 
+#+
+#+ and compiled with the function
+#+
 #+ callCopernicusCovariate()
-#+ 
-#+  
+#+
+#+
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
 #+
 #+ Authors:
 #+ - Max Lindmark
 #+ - Eros Quesada
-#+ 
+#+
 #+ Dev.notes:
-#+ 
-#+ - 20240507 Created. 
-#+ 
+#+
+#+ - 20240507 Created.
+#+
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
 extractCovariateAtLocation <- function(
-  covariate,              # Covariate to evaluate. Usually one of: sst, chlorophyll, elevation. Depends on input data.
-  locations_data,         # A df containing the set of year ("year") and locations ("lon", "lat") to be evaluated.
-  covariate_data,         # A df containing the covariate at location
-  changesYearly = c(0,1), # dichotomous
-  nametocov,              # A string indicating how to rename the covariate
-  messages = c(0,1)       # dichotomous
-){
-  
-  if(changesYearly == 0){
-    
-    # Inform on the process 
-    if(messages == 1){
+    covariate, # Covariate to evaluate. Usually one of: sst, chlorophyll, elevation. Depends on input data.
+    locations_data, # A df containing the set of year ("year") and locations ("lon", "lat") to be evaluated.
+    covariate_data, # A df containing the covariate at location
+    changesYearly = c(0, 1), # dichotomous
+    nametocov, # A string indicating how to rename the covariate
+    messages = c(0, 1) # dichotomous
+    ) {
+  if (changesYearly == 0) {
+    # Inform on the process
+    if (messages == 1) {
       cat(cyan("Processing"), "-", paste0("Gathering covariate information at location"), "\n")
     }
-    
+
     # Rename the df
     dfCovAtLocation <- locations_data
-    
-    # Create the covariate raster 
+
+    # Create the covariate raster
     cov_raster <- as_spatraster(covariate_data, xycols = 2:3, crs = "WGS84", digits = 2)
-    
+
     # Extract covariate value
     dfCovAtLocation$cov <- pull(terra::extract(cov_raster[covariate], dfCovAtLocation[, c("lon", "lat")])[2])
 
-    if(covariate == "elevation"){
-      
-      # Transform in strictly positive 
-      dfCovAtLocation$cov <- dfCovAtLocation$cov*(-1)
-      
-      # Inform on the process 
-      if(messages == 1){
+    if (covariate == "elevation") {
+      # Transform in strictly positive
+      dfCovAtLocation$cov <- dfCovAtLocation$cov * (-1)
+
+      # Inform on the process
+      if (messages == 1) {
         cat(cyan("Note"), "-", paste0("I turned the elevation values in strictly positives"), "\n")
       }
-      
     }
-    
   }
-  
-  if(changesYearly == 1){
-  
-  # Get the years 
-  listYearsAvailable <- sort(unique(locations_data$year))
-  
+
+  if (changesYearly == 1) {
+    # Get the years
+    listYearsAvailable <- sort(unique(locations_data$year))
+
 
     # Loop, make raster and extract
     covListAtLocation <- lapply(
-      listYearsAvailable, 
-      function(x){
-        
-        # Inform on the process 
-        if(messages == 1){
-          cat(cyan("Processing"), "-", paste0("Gathering covariate information at location for year ", which(listYearsAvailable == x),"/",length(listYearsAvailable), ":", x, "."), "\n")
+      listYearsAvailable,
+      function(x) {
+        # Inform on the process
+        if (messages == 1) {
+          cat(cyan("Processing"), "-", paste0("Gathering covariate information at location for year ", which(listYearsAvailable == x), "/", length(listYearsAvailable), ":", x, "."), "\n")
         }
-        
-        # Filter the year 
+
+        # Filter the year
         d_sub <- filter(locations_data, year == x)
         cov_tibble_sub <- filter(covariate_data, year == x)
-        
+
         # Convert covariate tibble to raster
-        cov_raster <- as_spatraster(cov_tibble_sub, xycols = 2:3,
-                                    crs = "WGS84", digits = 2)
-        
+        cov_raster <- as_spatraster(cov_tibble_sub,
+          xycols = 2:3,
+          crs = "WGS84", digits = 2
+        )
+
         # Extract at location from raster
-        d_sub$cov <- pull(terra::extract(cov_raster[covariate],
-                                         d_sub[, c("lon", "lat")])[2])
-        
-        # Return the df. 
+        d_sub$cov <- pull(terra::extract(
+          cov_raster[covariate],
+          d_sub[, c("lon", "lat")]
+        )[2])
+
+        # Return the df.
         d_sub
-        
-      })  
-  
-    # From list of tibbles to tibble 
+      }
+    )
+
+    # From list of tibbles to tibble
     dfCovAtLocation <- bind_rows(covListAtLocation)
-    
-    }
-  
-  # Rename the covariate based on user selection 
-  dfCovAtLocation <- dfCovAtLocation %>% 
+  }
+
+  # Rename the covariate based on user selection
+  dfCovAtLocation <- dfCovAtLocation %>%
     dplyr::rename(
       !!nametocov := cov
     )
-  
+
   # Inform completion
-  if(messages == 1){
-    cat("\n"); cat("\n")
+  if (messages == 1) {
+    cat("\n")
+    cat("\n")
     cat(green("Completed"))
   }
-  
+
   # Return resulting df
   dfCovAtLocation
-  
 }
-
-
-
-
-
-
-
-
-
-
-
--- R/functions/map-plot.R
+++ R/functions/map-plot.R
@@ -1,6 +1,6 @@
 library(sf)
 # Packages not on CRAN
-# devtools::install_github("seananderson/ggsidekick") # not on CRAN 
+# devtools::install_github("seananderson/ggsidekick") # not on CRAN
 library(ggsidekick)
 library(rnaturalearth)
 library(rnaturalearthdata)
@@ -10,17 +10,24 @@ theme_set(theme_sleek())
 sf::sf_use_s2(FALSE)
 
 # Specify map ranges
-ymin = 55.9; ymax = 59; xmin = 7.6; xmax = 13.3
+ymin <- 55.9
+ymax <- 59
+xmin <- 7.6
+xmax <- 13.3
 
 map_data <- rnaturalearth::ne_countries(
   scale = "large",
-  returnclass = "sf", continent = "europe")
+  returnclass = "sf", continent = "europe"
+)
 
 # Crop the polygon for plotting and efficiency:
 # st_bbox(map_data) # find the rough coordinates
 swe_coast <- suppressWarnings(suppressMessages(
-  st_crop(map_data,
-          c(xmin = xmin, ymin = ymin, xmax = xmax, ymax = ymax))))
+  st_crop(
+    map_data,
+    c(xmin = xmin, ymin = ymin, xmax = xmax, ymax = ymax)
+  )
+))
 
 # Transform our map into UTM 33 coordinates, which is the equal-area projection we fit in:
 utm_zone33 <- 32632
@@ -36,9 +43,9 @@ theme_facet_map <- function(base_size = 11, base_family = "") {
       legend.box.margin = margin(0, 0, 0, 0),
       legend.key.height = unit(0.4, "line"),
       legend.key.width = unit(2, "line"),
-      legend.spacing.x = unit(0.1, 'cm'),
-      #legend.position = "bottom",
-      legend.position = c(0.73, 0.04)
+      legend.spacing.x = unit(0.1, "cm"),
+      legend.position = "bottom",
+      # legend.position = c(0.73, 0.04)
     )
 }
 
@@ -52,24 +59,28 @@ ymax2 <- 6526000
 xrange <- xmax2 - xmin2
 yrange <- ymax2 - ymin2
 
-plot_map <- 
-  ggplot(swe_coast_proj) + 
+plot_map <-
+  ggplot(swe_coast_proj) +
   xlim(xmin2, xmax2) +
   ylim(ymin2, ymax2) +
   labs(x = "Longitude", y = "Latitude") +
-  geom_sf(size = 0.3, color = "gray80") + 
+  geom_sf(size = 0.3, color = "gray80") +
   theme_sleek() +
-  guides(colour = guide_colorbar(title.position = "top", title.hjust = 0.5),
-         fill = guide_colorbar(title.position = "top", title.hjust = 0.5)) +
+  guides(
+    colour = guide_colorbar(title.position = "top", title.hjust = 0.5),
+    fill = guide_colorbar(title.position = "top", title.hjust = 0.5)
+  ) +
   NULL
 
-plot_map_fc <- 
-  ggplot(swe_coast_proj) + 
+plot_map_fc <-
+  ggplot(swe_coast_proj) +
   xlim(xmin2, xmax2) +
   ylim(ymin2, ymax2) +
   labs(x = "Longitude", y = "Latitude") +
-  geom_sf(size = 0.3, color = "gray80") + 
+  geom_sf(size = 0.3, color = "gray80") +
   theme_facet_map() +
-  guides(colour = guide_colorbar(title.position = "top", title.hjust = 0.5),
-         fill = guide_colorbar(title.position = "top", title.hjust = 0.5)) +
+  guides(
+    colour = guide_colorbar(title.position = "top", title.hjust = 0.5),
+    fill = guide_colorbar(title.position = "top", title.hjust = 0.5)
+  ) +
   NULL
